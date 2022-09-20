1d5a6434f6aefe61af7dad6b84b0f724a9ac0419
hariskhants <haris.khan@techno-soft.com>
2022-09-20 20:02:15 +0500

    feat: dashboard layout

--- src/main/app/auth/logout/logout.component.ts
+++ src/main/app/auth/logout/logout.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@app/auth/services/auth.service';
-
+import { NavbarComponent } from '@app/dashboard/navbar/navbar.component';
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
